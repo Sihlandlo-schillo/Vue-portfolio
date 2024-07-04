7a4ea51e0f247e2f8692d254d66c3f95a8236232
sihlandlo-schillo <sihlandlom@gmail.com>
2024-07-04 07:57:16 +0200

    footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="footer">
-        <!-- <div>
+        <div>
             <p>&copy;{{ year }} by Sihlandlo Makhumsha</p>
         </div>
         <div>
             
-        </div> -->
+        </div>
     </div>
 </template>
 <script>
 export default {
-    // data() {
-    //     return {
-    //         year: new Date().getFullYear()
-    //     }
-    // }
+    data() {
+        return {
+            year: new Date().getFullYear()
+        }
+    }
 }
 </script>
 <style scoped>
