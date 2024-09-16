08c1fd99b325bfb44f7d4a64a1cc87fdf432f268
sihlandlo-schillo <sihlandlom@gmail.com>
2024-09-16 12:11:43 +0200

    router testimon. import

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResumeView from '../views/ResumeView.vue'
 import ContactView from '@/views/ContactView.vue'
-import TestimonialsView from '@/views/TestimonialsView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 
 const routes = [
